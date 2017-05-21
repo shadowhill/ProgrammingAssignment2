b9d892b428540b551967800795704fd6aae41f5d
shadowhill <tank2432331@163.com>
2017-05-21 22:46:58 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1.makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+## 2.cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
-  
-  
-  
+## 1st function gose to here:
+## give an objective matix.(random a n*n square matrix)
+## sample: x <- makeCacheMatrix(5) will generate a 5*5 square matrix.
+makeCacheMatrix <- function(n) {
+  x<-matrix(rnorm(n*n),n,n)
+  im <- NULL
+  set <-function(y,n) {
+    x <<- matrix(y,n,n)
+   im <<- NULL
+  }
+  get<-function() x
+  setinvm <-function(inversemartix) im <<-inversemartix
+  getinvm<-function() im
   
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean,
-       m=m)
+       setinvm = setinvm,
+       getinvm = getinvm)
 }
 
-
-## Write a short comment describing this function
+## 2nd function gose to here:
+## Inverse matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <-x$getinvm()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <-x$get()
+  m<-solve(data,...)
+  x$setinvm(m)
+  m
+  
 }
+
+## check matrix Functions:
+## A^-1 %*% A =I
+## checkMF<-function(x,y){
+##   round(x$get()%*%y,2)
+## }
